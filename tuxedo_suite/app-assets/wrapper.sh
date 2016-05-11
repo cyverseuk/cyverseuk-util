8ed685b2668331f9ee3ebdb4a8ed4661a0101ad7
erikvdbergh <erikvdbergh@gmail.com>
2016-05-11 16:04:29 +0100

    update app-assets

--- tuxedo_suite/app-assets/wrapper.sh
+++ tuxedo_suite/app-assets/wrapper.sh
@@ -1,5 +1,5 @@
 echo transfer_input_files = ${input1},${input2},${input3},${input4},${input5},${input6},${input7},${input8},${refgen},${refann},${maskfile},${maskfilec} >> lib/condorSubmitEdit.htc
-echo arguments=${lab1},${lab2},${lab3},${lab4},${indpre},${fastqscale},${libtype},${sensitivity},${insertsize},${stdev},${minanchor},${minintron},${maxintron},${maxalign},${minreadlength},${numthreads},${discalign},${mixalign},${usegtf},${userescue},${maxit},${idpre},${isofrac},${intraintrtresh},${maxintronu},${minintronz},${junctionalpha},${anchfrac},${mintransfrag},${termexonmax},${trimcovavg},${trimfrac},${transfraggap},${fragbias},${userescuecd},${testalign},${testfdr},${normalhits} >> condorSubmitEdit.htc
+echo arguments=${lab1},${lab2},${lab3},${lab4},${indpre},${fastqscale},${libtype},${sensitivity},${insertsize},${stdev},${minanchor},${minintron},${maxintron},${maxalign},${minreadlength},${numthreads},${discalign},${mixalign},${usegtf},${userescue},${maxit},${idpre},${isofrac},${intraintrtresh},${maxintronu},${minintronz},${junctionalpha},${anchfrac},${mintransfrag},${termexonmax},${trimcovavg},${trimfrac},${transfraggap},${fragbias},${userescuecd},${testalign},${testfdr},${normalhits} >> lib/condorSubmitEdit.htc
 cat lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
 jobid=`condor_submit lib/condorSubmitEdit.htc`
