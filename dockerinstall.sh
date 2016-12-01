f74e78229ab7ae8d29b4f32af99054cdbe41cbc6
Erik van den Bergh <erikvdbergh@gmail.com>
2016-12-01 18:00:47 +0000

    update to support xenial and some deps

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sudo apt install apt-transport-https ca-certificates
+
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo deb https://apt.dockerproject.org/repo ubuntu-wily main | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt update
 sudo apt -y install docker-engine
