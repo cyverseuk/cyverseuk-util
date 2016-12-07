7a60697975b94563f0557d06c73370e0c300e217
Erik van den Bergh <erikvdbergh@gmail.com>
2016-12-07 16:46:37 +0000

    add update command for fresh VMs

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+
 sudo apt install apt-transport-https ca-certificates
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
