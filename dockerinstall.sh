9d4a53c66d1f023e3750e8637d6d2b010a352606
Erik van den Bergh <erikvdbergh@gmail.com>
2017-01-19 16:59:35 +0000

    add -y for scripts

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install apt-transport-https ca-certificates
+sudo apt install -y apt-transport-https ca-certificates
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
